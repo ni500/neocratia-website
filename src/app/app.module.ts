06efac31df0ee4546ebcb8d706e235dea179c14c
Nicolás Contreras V <nicolas@pappcorn.com>
2019-05-07 10:29:34 -0400

    Neocratia site first deployment 🚀

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { CoreModule } from './core/core.module';
+import { PagesModule } from './pages/pages.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgMaterialModule } from './core/ng-material.module';
+import { RoutingRedirectGuard } from './core/routing-redirect.guard';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    AppRoutingModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    CoreModule,
+    NgMaterialModule,
+    PagesModule
   ],
-  providers: [],
+  // entryComponents: [RoutingRedirectGuard],
+  providers: [RoutingRedirectGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
